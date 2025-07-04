5303336f0d50fb81894bb0e234e81e3af4ae5003
SmitSolanki-303 <smitsolanki303@gmail.com>
2025-07-04 14:22:37 +0530

    tutorial-7 global-execution-context

---
+++ Tutorial-7/javascript/script.js
@@ -0,0 +1,93 @@
+// 1 - Global Execution context
+// 2 - This & window in global execution context
+
+
+
+// console.log(this); 
+// console.log(window);
+// console.log(myFunction);
+// console.log(fullName);
+// function myFunction() {
+//     console.log('hello world');
+// }
+
+// var fname= 'smit';
+// var lname= 'solanki';
+// var fullName = fname + ' ' + lname;
+// console.log(fullName);
+
+
+
+
+
+
+
+// 3 - Hoisting
+// Theory  - myFunction() is already been hoisted or stored in global memory before execution during the CREATION PHASE, so it can be called before its declaration.
+
+// console.log(this); 
+// console.log("myFunction() is already been hoisted");
+// myFunction();
+console.log(window);
+// console.log(myFunction);
+// console.log(fullName);
+// function myFunction() {
+//     console.log('hello world');
+// }
+
+// var fname= 'smit';
+// var lname= 'solanki';
+// var fullName = fname + ' ' + lname;
+// console.log(fullName);
+
+
+
+
+// 5 - Are let and const are hoisted? What is reference error?
+
+   
+console.log(myName);
+let  myName = 'Smit Solanki';
+console.log(myName);
+
+
+ 
+
+// TDZ - Temporal Dead Zone - It 
+
+
+// 6 - Function execution context
+
+
+
+
+
+// 7 - Scope chain & lexical environment
+
+
+
+
+
+// 8 - Intro to closure
+
+
+
+
+
+// 9 - Closuer Ex1
+
+
+
+
+
+// 10 - Closure Ex2
+
+
+
+
+
+// 11 - Closure Ex3
+
+
+
+
