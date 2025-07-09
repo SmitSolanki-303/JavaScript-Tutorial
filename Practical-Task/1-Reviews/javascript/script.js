c499ebabf79d1b96e961d6b702510f96d79d5f6e
SmitSolanki-303 <smitsolanki303@gmail.com>
2025-07-09 15:34:08 +0530

    Navbar webpage

--- Practical-Task/1-Reviews/javascript/script.js
+++ Practical-Task/1-Reviews/javascript/script.js
@@ -15,76 +15,64 @@ var surpriseBtn = document.querySelector(".primary-btn");
 // Select the first user
 let firstIndex = 0;
 
+function updateData(info) {
+  userImage.src = info.userImage;
+  infoTitle.textContent = info.fullname;
+  infoRole.textContent = info.role;
+  infoDescription.textContent = info.description;
+}
+
 // Using DOMContentLoaded, our first dynamic data would be access in HTML file.
 window.addEventListener("DOMContentLoaded", function () {
   const info = userData[firstIndex];
   console.log(info);
 
-  userImage.src = info.userImage;
-  infoTitle.textContent = info.fullname;
-  infoRole.textContent = info.role;
-  infoDescription.textContent = info.description;
+  updateData(info);
 
   prevBtn.addEventListener("click", function () {
     firstIndex--;
 
     if (firstIndex == -1) {
-      // console.log("-1");
       firstIndex = 3;
     }
 
-    console.log(firstIndex);
+    // console.log(firstIndex);
     const info = userData[firstIndex];
     console.log(info);
 
-    userImage.src = info.userImage;
-    infoTitle.textContent = info.fullname;
-    infoRole.textContent = info.role;
-    infoDescription.textContent = info.description;
+    updateData(info);
   });
 
   nextBtn.addEventListener("click", function () {
     firstIndex++;
 
     if (firstIndex === 4) {
-      // console.log(4);
       firstIndex = 0;
     }
 
-    console.log(firstIndex);
+    // console.log(firstIndex);
     const info = userData[firstIndex];
     console.log(info);
 
-    userImage.src = info.userImage;
-    infoTitle.textContent = info.fullname;
-    infoRole.textContent = info.role;
-    infoDescription.textContent = info.description;
+    updateData(info);
   });
 
   surpriseBtn.addEventListener("click", function () {
-    console.log("button click");
-    console.log(userData.length - 1);
 
     // get the random value
     let value = random(0, userData.length - 1);
-    console.log(value);
 
     // Convert the float value into integer value of random
     let randomValue = parseInt(value);
-    console.log(typeof randomValue);
 
-    console.log(info);
 
     for (let i = 0; i < userData.length; i++) {
       if (i === randomValue) {
-        console.log(i);
+
         const info = userData[i];
         console.log(info);
 
-        userImage.src = info.userImage;
-        infoTitle.textContent = info.fullname;
-        infoRole.textContent = info.role;
-        infoDescription.textContent = info.description;
+        updateData(info);
       }
     }
   });
