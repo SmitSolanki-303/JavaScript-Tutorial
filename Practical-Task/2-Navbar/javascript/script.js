0e4b388591b7353e5ed91dd8f8183729f41bca9f
SmitSolanki-303 <smitsolanki303@gmail.com>
2025-07-10 18:01:31 +0530

    Navbar design update

--- Practical-Task/2-Navbar/javascript/script.js
+++ Practical-Task/2-Navbar/javascript/script.js
@@ -4,10 +4,12 @@ var collapseLinks = document.querySelector(".collapse-links");
 
 hamburgerMenu.addEventListener("click", function () {
   if (collapseHeader.style.maxHeight) {
+    hamburgerMenu.style.transform = "rotate(0deg)";
     collapseHeader.style.maxHeight = null;
     collapseHeader.style.display = "none";
 
   } else {
+    hamburgerMenu.style.transform = "rotate(90deg)";
     collapseHeader.style.display = "inline-block";
     collapseHeader.style.maxHeight = collapseHeader.scrollHeight + "px";
   }
