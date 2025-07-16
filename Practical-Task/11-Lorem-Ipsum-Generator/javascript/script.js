cc2ec33283d1dbba7d3abebe0c0914c0be04c996
SmitSolanki-303 <smitsolanki303@gmail.com>
2025-07-16 15:20:17 +0530

    Lorem Generator Complete

--- Practical-Task/11-Lorem-Ipsum-Generator/javascript/script.js
+++ Practical-Task/11-Lorem-Ipsum-Generator/javascript/script.js
@@ -1,10 +1,42 @@
 import paragraphData from "./paragraphData.js";
-document.addEventListener("DOMContentLoaded", function(){
-    const form = document.querySelector(".input-form");
-    const limit = document.querySelector("#limit")
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".input-form");
+  const limit = document.querySelector("#limit");
+  var paragraph = document.querySelector(".paragraph-box");
 
-    form.addEventListener("submit", function(){
-        console.log("form submitted");
-        console.log(limit.value);
-    })
+  form.addEventListener("submit", function (e) {
+    // use to prevent the default behavior as after submit it disappear
+    e.preventDefault();
+    console.log("form submitted");
+    const value = parseInt(limit.value);
+    console.log(value);
+
+    const length = paragraphData.length;
+    //   console.log(length);
+
+    const random = Math.floor(Math.random() * length);  
+
+
+    //  (isNaN(value) || value <= 0 || value > length)
+    // for(let i = value ; i > 0 ; i++) {
+    //     paragraph.innerHTML = `<p class="paragraph">${paragraphData[i]}</p>`;
+    // }
+
+    if (isNaN(value) || value <= 0 ) {
+      paragraph.innerHTML = `<p class="paragraph">${paragraphData[random]}</p>`;
+    } else {
+      // slice(startingIndex , lastIndex)
+      let tempParagraph = paragraphData.slice(0, value);
+      console.log(tempParagraph);
+
+      tempParagraph = tempParagraph.map(function (paragraph) {
+          // console.log(paragraph);
+          return `<p class="paragraph">${paragraph}</p>`;
+        })
+        .join("");
+
+      paragraph.innerHTML = tempParagraph;
+
+    }
+  });
 });
